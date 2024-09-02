2ef1565090832642ef6fa2d1f43e12b7ddb2baa9
Joey Zhang <zjoey049@gmail.com>
2024-09-02 16:41:11 -0400

    clean up folder and add gitignore

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/joeyzhang/Documents/git/metronal
+# Install script for directory: /Users/joeyzhang/Documents/git/term-metronome
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/joeyzhang/Documents/git/metronal/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/joeyzhang/Documents/git/term-metronome/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
